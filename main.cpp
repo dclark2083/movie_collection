038b6ef8de296fbf899a41bf2b4e48db5cbb0a92
Darren Clark <darclar@ut.utm.edu>
2022-10-11 13:48:15 -0500

    Added file comments to main.cpp.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
+/**
+ * @file main.cpp
+ * @author Darren Clark & Kyle Byassee
+ * @date 2022-10-11
+ * @brief This program maintains a movie collection database.
+ * 
+ * This program provides users with an interface presenting several
+ * options to manage their movie collection. They can add, remove, 
+ * search the database.
+ */
+
 
 #include <iostream>
 
 using namespace std;
 
 int main() {
-  cout << "hello." << endl;
-  cout << "hello yourself" << endl;
+
   return 0;
 }
