754df6b19d0471f93b5c2bcbf1f6e35aaf5bd9bf
Kyle Byassee <kylhbyas@ut.utm.edu>
2022-10-27 12:36:01 -0500

    Required Files

---
+++ library.cpp
@@ -0,0 +1,23 @@
+#include "library.h"
+
+using namespace std;
+
+void library::insert_sorted() {
+
+}
+
+void library::lookup() {
+
+}
+
+void library::reverse_lookup() {
+
+}
+
+void library::director_search() {
+
+}
+
+void library::delete_user() {
+
+}
---
+++ library.h
@@ -0,0 +1,15 @@
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+class library {
+ private:
+  int thing = 0;
+
+ public:
+  void insert_sorted();
+  void lookup();
+  void reverse_lookup();
+  void director_search();
+  void delete_user();
+};
+#endif //LIBRARY_H
